03cb02d87c52b343eacf7c5f9c627445692a520f
Igor Pavlov <igortvk@ya.ru>
2021-01-29 18:59:50 +0300

    added steps

--- src/test/java/tests/LendingPageTests.java
+++ src/test/java/tests/LendingPageTests.java
@@ -2,12 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import helpers.ConfigHelper;
-import io.qameta.allure.Step;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
@@ -17,9 +15,10 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
+import static io.qameta.allure.Allure.step;
 
 public class LendingPageTests {
-    final String SUCCESSFUL_TEXT = "СПАСИБО ЗА ВАШУ ИДЕЮ, ОНА ПОПАДЁТ В СООТВЕТСТВУЮЩЕЕ НАПРАВЛЕНИЕ И МЫ ОБЯЗАТЕЛЬНО РАССМОТРИМ ЕЁ!";
+    private final String SUCCESSFUL_TEXT = "СПАСИБО ЗА ВАШУ ИДЕЮ, ОНА ПОПАДЁТ В СООТВЕТСТВУЮЩЕЕ НАПРАВЛЕНИЕ И МЫ ОБЯЗАТЕЛЬНО РАССМОТРИМ ЕЁ!";
 
     @BeforeAll
     static void setup() {
@@ -30,49 +29,78 @@ public class LendingPageTests {
         capabilities.setCapability("enableVideo", true);
         Configuration.remote = ConfigHelper.getURL();
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @Feature("Test for Sibur Digital")
+    @Owner("Pavlov Igor")
+    @Test
+    @DisplayName("Проверить успешность отправки сообщения в разделе Идея")
+    public void ShouldCheckIdeasPageBlockIdea() {
+        step("Открыть страницу и перейти в раздел Ideas", () -> {
+            open("https://sibur.digital/ideas");
+            $(".page-ideas__type_ideas").click();
+        });
+        step("Ввести данные в форму обратной связи и нажать отправить", () -> {
+            $("[placeholder='Имя']").setValue("Test").pressTab();
+            $("[placeholder='E-mail']").setValue("test@test.com").pressTab();
+            $("[placeholder='Тема сообщения']").setValue("Attention").pressTab();
+            $("[placeholder='Текст сообщения']").setValue("I have a Lot of Ideas").pressTab();
+            $("[type='submit']").pressEnter();
+        });
+        step("Проверить, что сообщение успешно отправилось", () -> {
+            $("div.ideas-form__form-container").shouldHave(text(SUCCESSFUL_TEXT));
+        });
+    }
 
-    }
-    @DisplayName("Проверить успешную отправку сообщения в разделе Идея")
     @Test
-    void ShouldCheckIdeasPageBlockIdea() {
-        open("https://sibur.digital/ideas");
-        $(".page-ideas__type_ideas").click();
-        $("[placeholder='Имя']").setValue("Test").pressTab();
-        $("[placeholder='E-mail']").setValue("test@test.com").pressTab();
-        $("[placeholder='Тема сообщения']").setValue("Attention").pressTab();
-        $("[placeholder='Текст сообщения']").setValue("I have a Lot of Ideas").pressTab();
-        $("[type='submit']").pressEnter();
-        $("div.ideas-form__form-container").shouldHave(text(SUCCESSFUL_TEXT));
+    @DisplayName("Проверить успешность отправки сообщения в разделе Сотрудничество, где имя < 3 символов")
+    public void ShouldCheckIdeasPageBlockCooperation() {
+        step("Открыть сайт и перейти в рздел Сотрудниество", () -> {
+            open("https://sibur.digital/ideas");
+            $(".page-ideas__type_coop").click();
+        });
+        step("Ввести данные в форму обратной связи и нажать отправить", () -> {
+            $("[placeholder='Имя']").setValue("T").pressTab();  /* Некорректное имя */
+            $("[placeholder='E-mail']").setValue("test@hotmail.com").pressTab();
+            $("[placeholder='Тема сообщения']").setValue("Attention").pressTab();
+            $("[placeholder='Текст сообщения']").setValue("I can make the digital world better").pressTab();
+            $("[type='submit']").pressEnter();
+        });
+        step("Проверить, что письмо о Сотрудничестве успешно отправилось", () -> {
+            $("div.ideas-form__form-container").shouldHave(text(SUCCESSFUL_TEXT));
+        });
     }
-    @DisplayName("Проверить успешную отправку сообщения в разделе Соотрудничество, где имя < 3 символов")
+
     @Test
-    void ShouldCheckIdeasPageBlockCooperation() {
-        open("https://sibur.digital/ideas");
-        $(".page-ideas__type_coop").click();
-        $("[placeholder='Имя']").setValue("T").pressTab();  /* Некорректное имя */
-        $("[placeholder='E-mail']").setValue("test@hotmail.com").pressTab();
-        $("[placeholder='Тема сообщения']").setValue("Attention").pressTab();
-        $("[placeholder='Текст сообщения']").setValue("I can make the digital world better").pressTab();
-        $("[type='submit']").pressEnter();
-        $("div.ideas-form__form-container").shouldHave(text(SUCCESSFUL_TEXT));
+    @DisplayName("Проверить, что пользователь успешно подписался на рассылку")
+    public void ShouldCheckSuccessfulSubscription() {
+        step("Открыть сайт проскролить вниз и перейти в раздел 'Подписаться на рассылку'", () -> {
+            open("https://sibur.digital/about");
+            $(".subscribe__text").scrollIntoView(true).click();
+        });
+        step("Ввести почту, согласиться с правилами и подписаться", () -> {
+            $("[placeholder='e-mail']").setValue("test@test.com");
+            $(".subscribe-popup__agreement-checkbox").click();
+            $(byText("подписаться")).click();
+        });
+            step("Проверить, что пользователь успешно подписался", () -> {
+            $(".subscribe-popup__success-text").shouldHave(text("ВЫ УСПЕШНО ПОДПИСАЛИСЬ"));
+        });
     }
     @Test
-    void ShouldCheckSuccessfulSubscription() {
-        open("https://sibur.digital/about");
-        $(".subscribe__text").scrollIntoView(true).click();
-        $("[placeholder='e-mail']").setValue("test@test.com");
-        $(".subscribe-popup__agreement-checkbox").click();
-        $(byText("подписаться")).click();
-        $(".subscribe-popup__success-text").shouldHave(text("ВЫ УСПЕШНО ПОДПИСАЛИСЬ"));
-    }
-    @Test
-    void ShouldCheckSuccessfulSearchInfo() {
-        open("https://sibur.digital/");
-        $(".nav__circles").click();
-        $("[placeholder='поиск']").setValue("Agile");
-        $(".search__result-item-category").click();
+    @DisplayName("Проверить успешность поиска по сайту")
+    public void ShouldCheckSuccessfulSearchInfo() {
+        step("Открыть сайт и перейти в раздел поиска по сайту ", () -> {
+            open("https://sibur.digital/");
+            $(".nav__circles").click();
+        });
+        step("Ввести данные для поиска, например 'Agile' и выполнить поиск", () -> {
+            $("[placeholder='поиск']").setValue("Agile");
+            $(".search__result-item-category").click();
+        });
+        step("Проверить, что поиск выполнился успешно", () -> {
         $(".article-header__title-text").should(visible);
+        });
     }
 
     @AfterEach
@@ -85,4 +113,3 @@ public class LendingPageTests {
         closeWebDriver();
     }
 }
-
